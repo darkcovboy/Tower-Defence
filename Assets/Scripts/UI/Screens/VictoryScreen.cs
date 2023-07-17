ca1b65427de98d8a59eaf58ba8b3a3275474b947
darkcovboy <47691903+darkcovboy@users.noreply.github.com>
2023-07-18 00:38:52 +0300

    Instruction

--- Assets/Scripts/UI/Screens/VictoryScreen.cs
+++ Assets/Scripts/UI/Screens/VictoryScreen.cs
@@ -38,10 +38,7 @@ public class VictoryScreen : Screen
 
     public void SetStars(int stars)
     {
-        for (int i = 0; i < stars; i++)
-        {
-            _stars[i].Activate();
-        }
+        _countStars = stars;
     }
 
     IEnumerator ShowStars()
